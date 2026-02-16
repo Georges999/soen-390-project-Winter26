4fa6f99cfb42df3bc01305fd176e9c367670fb16
Peter Samaha <petersamaha2004@gmail.com>
2026-02-15 19:08:48 -0500

    changed jest config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   preset: 'jest-expo',
+  rootDir: '.',
   setupFilesAfterEnv: ['./jest.setup.js'],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    'App.js',
+    'app.config.js',
+    '!**/__tests__/**',
+    '!**/node_modules/**',
+  ],
+  coverageDirectory: 'coverage',
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
   ]
