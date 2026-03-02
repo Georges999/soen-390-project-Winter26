cd03700391637a19969cda65b4de28082f5714d9
Samer-Hasna <150304335+Samer-Hasna@users.noreply.github.com>
2026-03-01 19:54:59 -0500

    Updating test coverage settings
    Updated coverage settings from Jest config.

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -10,16 +10,7 @@ module.exports = {
     '!**/node_modules/**',
   ],
   coverageDirectory: 'coverage',
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80
-    },
-    // optionally tighten per-file or per-directory thresholds
-    // "src/components/": { branches: 90, functions: 90 },
-  },
+  
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
   ]
